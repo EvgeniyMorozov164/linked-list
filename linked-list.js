23021480f12ec55f1b5496f8c23d413c4ace90ce
Evgeniy Morozov <131901919+EvgeniyMorozov164@users.noreply.github.com>
2024-10-10 11:16:10 +0400

    add at(index) method

--- linked-list.js
+++ linked-list.js
@@ -15,7 +15,7 @@ class LinkedList {
   append(value) {
     const node = new Node(value);
 
-    if (!this.head || !this.tail) {
+    if (this.length === 0) {
       this.head = node;
       this.tail = node;       
     }
@@ -36,7 +36,7 @@ class LinkedList {
     if (!this.tail) {
       this.tail = node;
     }
-    
+
     this.length++;
 
     return this;
@@ -45,7 +45,37 @@ class LinkedList {
   size() {
     return this.length;
   }
+  
+  printHead() {
+    return this.head;
+  }
 
+  printTail() {
+    return this.tail;
+  }
+
+  at(index) {
+
+    if (this.length === 0 || this.length < index || index < 0) {
+      return null;
+    }
+
+    if (index === 0) {
+      return this.head;
+    }
+
+    let current = this.head;
+    let count = 0;
+
+    while (count < index) {
+      current = current.next;
+      count++;
+      if (count === index) {
+        return current;
+      }
+    }
+
+  }
 }
 
 let list = new LinkedList();
@@ -54,6 +84,10 @@ list.append(2);
 list.append(3);
 list.append(4);
 list.append(8);
-list.prepend(9);
-console.log(list);
-console.log(list.size());
+// list.prepend(9);
+// list.prepend(10);
+// console.log(JSON.stringify(list));
+// console.log(list.size());
+// console.log(list.printHead());
+// console.log(list.printTail());
+console.log(list.at(1));
