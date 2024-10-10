b243700e301a9dbddf9414baf3b8e0137fdd3114
Evgeniy Morozov <131901919+EvgeniyMorozov164@users.noreply.github.com>
2024-10-10 10:23:29 +0400

    add append and prepend methods

--- linked-list.js
+++ linked-list.js
@@ -13,19 +13,39 @@ class LinkedList {
   }
 
   append(value) {
-    let node = new Node(value);
+    const node = new Node(value);
 
-    if(!this.head) {
+    if (!this.head || !this.tail) {
       this.head = node;
-    } else {
-      this.tale = node;
+      this.tail = node;       
     }
+
+    this.tail.next = node;    
+    this.tail = node;
     this.length++;
+
+    return this;
+  
+  } 
+  
+  prepend(value) {
+    const node = new Node(value);
+    node.next = this.head;
+    this.head = node;
+
+    if (!this.tail) {
+      this.tail = node;
+    }
+    
+    this.length++;
+
+    return this;
   }
 
   size() {
     return this.length;
   }
+
 }
 
 let list = new LinkedList();
@@ -33,5 +53,7 @@ list.append(1);
 list.append(2);
 list.append(3);
 list.append(4);
+list.append(8);
+list.prepend(9);
 console.log(list);
 console.log(list.size());
